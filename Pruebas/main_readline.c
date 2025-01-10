647b6863d055d05f47aefb969e5825808a06d615
HormidDev <ide-dieg@student.42madrid.com>
2025-01-11 00:35:55 +0100

    añadido submodulo de la libft e iniciando pruebas

---
+++ Pruebas/main_readline.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_readline.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 21:37:02 by ide-dieg          #+#    #+#             */
+/*   Updated: 2025/01/10 22:52:34 by ide-dieg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../42_Libft/libft.h"
+
+int main()
+{
+	char *line;
+
+
+	while (1)
+	{
+		line = readline("prompt> ");
+		printf("line: %s\n", line);
+		if (!line)
+			break;
+		if (ft_strncmp_p(line, "re", 2) == 0)
+		{
+			sleep(1);
+			rl_replace_line("adios", 0);
+			rl_redisplay();
+			sleep(1);
+		}
+		add_history(line);
+		printf("line: %s\n", line);
+		free(line);
+	}
+	rl_clear_history();
+	printf("History cleared\n");
+	return (0);
+}
