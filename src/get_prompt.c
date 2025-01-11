d65830938d09c2384590d8f27972622cb412d343
Daniel Gimeno De Manuel <dagimeno@c2r19s5.42madrid.com>
2025-01-11 15:52:41 +0100

    funcion get_current anadida

--- src/get_prompt.c
+++ src/get_prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_prompt.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
+/*   By: dagimeno <dagimeno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 13:59:51 by ide-dieg          #+#    #+#             */
-/*   Updated: 2025/01/11 14:36:49 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2025/01/11 15:51:31 by dagimeno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,25 @@ static char	*get_hostname(void)
 	split2 = ft_split_ae(split[1], '.');
 	return (ft_strdup_ae(split2[0]));
 }
+static char	*get_current(void)
+{
+	char	*cwd;
+	char	**split;
+	char	*join;
+	int		i;
+
+	cwd = getcwd(NULL, 0);
+	if (!ft_strncmp_p(cwd, "home/", ft_strlen_p(cwd)) || !ft_strncmp_p(cwd, "/",  ft_strlen_p(cwd)))
+		return (cwd);
+	split = ft_split_ae(cwd, '/');
+	if (!split[2])
+		return ("~");
+	join = ft_strjoin_p("~/", split[2]);
+	i = 3;
+	while (split[i])
+		join = ft_strjoin_p(join, split[i++]);
+	return (join);
+}
 
 char	*get_prompt(void)
 {
@@ -35,7 +54,7 @@ char	*get_prompt(void)
 	ft_printf("user_name: %s\n", user_name);
 	host_name = get_hostname();
 	ft_printf("host_name: %s\n", host_name);
-	cwd = getcwd(NULL, 0);
+	cwd = get_current();
 	ft_printf("cwd: %s\n", cwd);
 	ft_sprintf(prompt, "%s%s@%s%s:%s%s%s> %s", BOLD, user_name, host_name, RESET, YELLOW, cwd, RESET, RESET);
 	free(cwd);
