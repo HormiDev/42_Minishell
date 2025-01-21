6432330c4b0e0e7e3a988cf3a54c89b58bb71139
= <=>
2025-01-21 13:26:12 +0100

    update prompt corregido

--- src/ft_update_prompt.c
+++ src/ft_update_prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_update_prompt.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
+/*   By: dagimeno <dagimeno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 19:52:44 by ide-dieg          #+#    #+#             */
-/*   Updated: 2025/01/20 22:28:06 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2025/01/21 13:15:48 by dagimeno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 /**
  * @brief Actualiza el prompt del minishell modificando el cwd_short en caso de
- * que el home exista en el envairoment y el inicio del cwd sea igual al home
+ * que el home exista en el enviroment y el inicio del cwd sea igual al home
  * 
  * @param minishell Estructura del minishell
  * @return void
  */
-void	ft_update_prompt(t_minishell *minishell)//revisar esta funcion
+void	ft_update_prompt(t_minishell *minishell)
 {
 	char	*home;
 
 	home = ft_getenv("HOME", minishell->envp);
-	if (!home && ft_strncmp_p(home, minishell->cwd, ft_strlen_p(home)))
+	if (home && ft_strncmp_p(home, minishell->cwd, ft_strlen_p(home)))
 		ft_strlcpy(minishell->cwd_short, minishell->cwd, PATH_MAX);
 	else
 		ft_sprintf(minishell->cwd_short, "~%s",
