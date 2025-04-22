3840cab355c7e5f6ce5190b2dd149f59d2163e41
Furiosso <dgimenodemanuel@gmail.com>
2025-04-22 23:24:47 +0200

    main.c normalizado

--- src/main.c
+++ src/main.c
@@ -12,17 +12,8 @@
 
 #include "../include/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+static void	ft_main_loop(t_minishell *minishell)
 {
-	t_minishell *minishell;
-
-	if (argc != 1 || !argv)
-		return (ft_putstr_fd("Error: invalid arguments\n", 2), 1);
-	minishell = ft_loading_minishell(envp);
-	ft_config_signals();
-	//ft_export("USER_ZDOTDIR=hola que tal estamos", &minishell->envp);
-	//ft_export("TARTA=hola que tal estamos", &minishell->envp);
-	//ft_unset("LS_COLORS", &minishell->envp);
 	while (1)
 	{
 		ft_update_prompt(minishell);
@@ -31,10 +22,10 @@ int	main(int argc, char **argv, char **envp)
 		if (minishell->line)//no necesario para readline, revisar
 			minishell->line[ft_strlen_p(minishell->line) - 1] = '\0';//no necesario para readline*/
 		if (!minishell->line)
-		{	
+		{
 			minishell->line = ft_strdup_ae("exit");
 			ft_parsing_and_exec(minishell);
-		}	
+		}
 		else
 			ft_add_to_alloc_lst_e(minishell->line);
 		if (*minishell->line)
@@ -43,10 +34,20 @@ int	main(int argc, char **argv, char **envp)
 			ft_dprintf(minishell->history_fd, "%s\n", minishell->line);
 			ft_parsing_and_exec(minishell);
 		}
-		ft_free_alloc((minishell->line));//en caso de lentitud eliminar
+		ft_free_alloc((minishell->line));
 		ft_config_signals();
 	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_minishell	*minishell;
+
+	if (argc != 1 || !argv)
+		return (ft_putstr_fd("Error: invalid arguments\n", 2), 1);
+	minishell = ft_loading_minishell(envp);
+	ft_config_signals();
+	ft_main_loop(minishell);
 	rl_clear_history();
 	ft_alloc_lst(0, 0);
-	return (0);
 }
