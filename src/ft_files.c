93d2d44498f0f82ccab7b2753d26544d202c4fa1
Furiosso <dgimenodemanuel@gmail.com>
2025-04-22 20:05:47 +0200

    ft_files.c normalizado menos unos comentarios de una parte que hay que revisar

--- src/ft_files.c
+++ src/ft_files.c
@@ -12,7 +12,7 @@
 
 #include "../include/minishell.h"
 
-int	ft_open_files(t_cmd *cmd)
+static int	ft_open_infiles(t_cmd *cmd)
 {
 	int	i;
 
@@ -22,7 +22,8 @@ int	ft_open_files(t_cmd *cmd)
 		if (cmd->io_fd[0] > 0)
 			close(cmd->io_fd[0]);
 		if (((t_redir *)(cmd->infiles[i]))->type == 0)
-			cmd->io_fd[0] = open(((t_redir *)(cmd->infiles[i]))->file, O_RDONLY);
+			cmd->io_fd[0] = open(((t_redir *)
+						(cmd->infiles[i]))->file, O_RDONLY);
 		if (cmd->io_fd[0] < 0)
 		{
 			perror(cmd->infiles[i]->file);//revisar
@@ -31,15 +32,24 @@ int	ft_open_files(t_cmd *cmd)
 		}
 		i++;
 	}
+	return (0);
+}
+
+static int	ft_open_outfiles(t_cmd *cmd)
+{
+	int	i;
+
 	i = 0;
 	while (cmd->outfiles[i])
 	{
 		if (cmd->io_fd[1] > 1)
 			close(cmd->io_fd[1]);
 		if (((t_redir *)(cmd->outfiles[i]))->type == 1)
-			cmd->io_fd[1] = open(((t_redir *)(cmd->outfiles[i]))->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			cmd->io_fd[1] = open(((t_redir *)(cmd->outfiles[i]))->file,
+					O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		else if (((t_redir *)(cmd->outfiles[i]))->type == 2)
-			cmd->io_fd[1] = open(((t_redir *)(cmd->outfiles[i]))->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			cmd->io_fd[1] = open(((t_redir *)(cmd->outfiles[i]))->file,
+					O_WRONLY | O_CREAT | O_APPEND, 0644);
 		if (cmd->io_fd[1] < 0)
 		{
 			perror(cmd->outfiles[i]->file);//revisar
@@ -48,6 +58,15 @@ int	ft_open_files(t_cmd *cmd)
 		}
 		i++;
 	}
+	return (0);
+}
+
+int	ft_open_files(t_cmd *cmd)
+{
+	if (ft_open_infiles(cmd) == -1)
+		return (-1);
+	if (ft_open_outfiles(cmd) == -1)
+		return (-1);
 	return (0);
 }
 
